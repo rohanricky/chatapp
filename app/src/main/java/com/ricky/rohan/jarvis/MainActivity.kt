6a29e6f7a298516981269ef79d8dd5e3a5b2637c
rohanricky <rohanricky0609@gmail.com>
2017-11-18 23:15:59 +0530

    fuel http

--- app/src/main/java/com/ricky/rohan/jarvis/MainActivity.kt
+++ app/src/main/java/com/ricky/rohan/jarvis/MainActivity.kt
@@ -9,6 +9,10 @@ import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
 import android.widget.Toast
+import com.github.kittinunf.fuel.Fuel
+import com.github.kittinunf.fuel.httpGet
+import com.github.kittinunf.result.Result
+import com.github.kittinunf.result.getAs
 import kotlinx.coroutines.experimental.CommonPool
 import kotlinx.coroutines.experimental.async
 import java.io.BufferedReader
@@ -21,38 +25,32 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
+        val edit = findViewById<View>(R.id.simpleEditText) as  EditText
         val button = findViewById<Button>(R.id.button) as Button
+        val text = findViewById<View>(R.id.text) as TextView
         button.setOnClickListener {
-            val job = async(CommonPool) {
-                //val result = URL("http://personalssistantv1.firebaseapp.com/api").readText()
-                val url = "https://personalssistantv1.firebaseapp.com/api"
-                val obj = URL(url)
-                val response = StringBuffer()
-                with(obj.openConnection() as HttpURLConnection, {
-                    // optional default is GET
-                    requestMethod = "GET"
-
-
-                    println("\nSending 'GET' request to URL : $url")
-                    println("Response Code : $responseCode")
-
-                    BufferedReader(InputStreamReader(inputStream)).use {
-
-                        var inputLine = it.readLine()
-                        while (inputLine != null) {
-                            inputLine = it.readLine()
-                            response.append(inputLine)
-                        }
-                    }
-                    println(response.toString())
-                })
-                runOnUiThread() {
-                    Toast.makeText(this@MainActivity,response, Toast.LENGTH_SHORT).show()
+            Fuel.post("https://crackiit.online/api",listOf("data" to "Rohan")).body("{data:\"Rohan\"}").response{request, response, result ->
+                val (value, error) = result
+                if (error == null) {
+                    text.setText(value.toString())
+                    //Toast.makeText(this@MainActivity,value.toString(), Toast.LENGTH_SHORT).show()
+                    println(value)
+                } else {
+                    //error handling
                 }
             }
-        }
-        /*fun SendMethod(view : View) {
+            /*"https://crackiit.online/api".httpGet().responseString { request, response, result ->
+                //make a GET to http://httpbin.org/get and do something with response
+                val (data, error) = result
+                if (error == null) {
+                    text.setText(data)
+                    println(data)
+                } else {
+                    //error handling
+                }
+            }*/
+
+            /*fun SendMethod(view : View) {
             var req = findViewById<View>(R.id.simpleEditText) as EditText
             val url = "http://personalssistantv1.firebaseapp.com/api"
             val obj = URL(url)
@@ -78,6 +76,34 @@ class MainActivity : AppCompatActivity() {
                 }
             })
         }*/
+        }
     }
 }
+/*async(CommonPool) {
+                //val result = URL("http://personalssistantv1.firebaseapp.com/api").readText()
+                val url = "https://crackiit.online/api"
+                val obj = URL(url)
+                val response = StringBuffer()
+                with(obj.openConnection() as HttpURLConnection, {
+                    // optional default is GET
+                    requestMethod = "GET"
 
+
+                    println("\nSending 'GET' request to URL : $url")
+                    println("Response Code : $responseCode")
+
+                    BufferedReader(InputStreamReader(inputStream)).use {
+
+                        var inputLine = it.readLine()
+                        while (inputLine != null) {
+                            inputLine = it.readLine()
+                            response.append(inputLine)
+                        }
+                    }
+                    println(response.toString())
+                })
+                runOnUiThread() {
+                    Toast.makeText(this@MainActivity,response.toString(), Toast.LENGTH_SHORT).show()
+                }
+            }
+        }*/
